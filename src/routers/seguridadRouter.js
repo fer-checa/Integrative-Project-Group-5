e4f50a9dab91abf873e199d621330f161b62d189
adolfowirth <adolfowirth@hotmail.com>
2022-06-08 17:57:51 -0300

    agregar usuario a la bd y editar usuario

--- src/routers/seguridadRouter.js
+++ src/routers/seguridadRouter.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const indexController = require('../controllers/seguridadControllers');
+const seguridadController = require('../controllers/seguridadControllers');
 
-router.get('/',indexController.login);
+router.get('/login',seguridadController.login);
 
-router.get('/register',indexController.register);
+router.get('/register',seguridadController.register);
+router.post('/register',seguridadController.create);
 
-router.get('/admin',indexController.admin);
+router.get('/edit/:id', seguridadController.edit); 
+router.patch('/edit/:id', seguridadController.update); 
+
+/* router.delete('/delete/:id', seguridadController.destroy); */
+ 
+router.get('/admin',seguridadController.admin); 
 
 
 module.exports = router;
