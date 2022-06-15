7a65aef7aaec716fff179181668cb5a3fb60c15d
mamenendez <mamenendez@gmail.com>
2022-06-14 23:20:28 -0300

    crud productos

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -6,7 +6,9 @@ const adminController = require('../controllers/adminController');
 
 router.get('/',adminController.index);
 
-router.get('/product',adminController.product);
+
+
+
 
 
 module.exports = router;
--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -10,5 +10,10 @@ router.get('/productDetail/:id',productsController.productDetail);
 router.get('/sucursales',productsController.sucursales);
 router.get('/todosLosProductos',productsController.todosLosProductos);
 
+router.get('/productAdmin',productsController.productAdmin);
+router.get('/productNew',productsController.productNew);
+router.get('/productEdit',productsController.productEdit);
+
+router.post('/productDelete/:id', productsController.destroy);
 
 module.exports = router;
