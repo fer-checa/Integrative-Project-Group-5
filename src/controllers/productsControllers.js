530d3a47c81e5242a1b3012e4c8416e0193301d5
mamenendez <mamenendez@gmail.com>
2022-06-17 15:40:55 -0300

    arreglos varios

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -8,29 +8,33 @@ const productsController =
     index: (req, res) => {
         res.render('index',{titulo:'Mundo Mascota DH'});  
 	},
+
     todosLosProductos: (req, res) => 
     {
-        const productsFilePath = path.join(__dirname, '../data/products.json');
-        res.render('todosLosProductos',{titulo:'Mundo Mascota DH-Productos',productsFilePath})        
-    },
-    productDetail: (req, res) => {
-        const productsFilePath = path.join(__dirname, '../data/products.json');
-        const todosLosProductos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
         
-		let id = req.params.id
+        const todosLosProductos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        res.render('todosLosProductos',{titulo:'Mundo Mascota DH-Productos',todosLosProductos})        
+    },
+
+    productDetail: (req, res) => {
+        
+        const todosLosProductos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+         let id = req.params.id
 		let product = todosLosProductos.find(product => product.id == id)
-		res.render('productDetail',{titulo:'Mundo Mascota DH-Detalle Producto', product})
+       res.render('productDetail',{titulo:'Mundo Mascota DH-Detalle Producto', product})
         
 	}, 
+
     productCart: (req, res) => 
     {
         res.render('productCart', {titulo:'Mundo Mascota DH-Carrito'});   
-            
     },
+
     sucursales: (req, res) => 
     {
         res.render('sucursales',{titulo:'Mundo Mascota DH-Sucursales'});        
     },
+
     productAdmin : (req,res) => 
     {
         const todosLosProductos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
