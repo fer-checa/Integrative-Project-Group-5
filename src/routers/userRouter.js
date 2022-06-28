4e17accc2ee9c323e53748f2b9f7a87c7c44bbd4
adolfowirth <adolfowirth@hotmail.com>
2022-06-27 21:45:37 -0300

    agregando multer a users

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const {body, check} = require('express-validator');
+const path = require('path');
+//
+const multer = require('multer');
+
+//multer
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "public/img/usersImage")
+    },
+    filename: function(req, file, cb){
+        cb(null, "user-" + Date.now() + path.extname(file.originalname))
+    },
+    
+})
+
+const upload = multer({storage: storage});
+
 
 const validarDatos = [
     body('name').notEmpty().withMessage('Debes Completar el Nombre').bail(),
     body('email').notEmpty().withMessage('Debes Completar el email').bail().isEmail().withMessage('Debes completar un email valido'),
-    body('password').notEmpty().withMessage('Debes Completar la contraseña').bail().isLength({min: 8}).withMessage('La constraseña debe tener como minimo 8 caracteres'),
-    body('rePassword').notEmpty().withMessage('Debes Completar Repetir la contraseña').bail().isLength({min: 8}).withMessage('La constraseña debe tener como minimo 8 caracteres')
+    body('password').notEmpty().withMessage('Debes Completar la contraseña').bail().isLength({min: 4}).withMessage('La constraseña debe tener como minimo 8 caracteres'),
+    body('rePassword').notEmpty().withMessage('Debes Completar Repetir la contraseña').bail().isLength({min: 4}).withMessage('La constraseña debe tener como minimo 8 caracteres')
 ]
 
 const userController = require('../controllers/userControllers');
@@ -19,11 +37,11 @@ check('password').isLength({min: 8}).withMessage('Contraseña Incorrecta')], use
 
 /* REGISTRACION */
 router.get('/register',userController.register);
-router.post('/register', validarDatos ,userController.create);
+router.post('/register', upload.single("product-image"),validarDatos ,userController.create);
 
 router.get('/list', userController.list);
 router.get('/edit/:id', userController.edit); 
-router.patch('/edit/:id', userController.update); 
+router.patch('/edit/:id',upload.single("product-image"), userController.update); 
 router.delete('/delete/:id', userController.destroy);
 
 module.exports = router;
