e8929121154cd10105210bf263452de4bf8091a3
mamenendez <mamenendez@gmail.com>
2022-07-02 18:50:26 -0300

    ejs no autorizado, logica en middleware admin

--- src/middlewares/userRouteAdminMW.js
+++ src/middlewares/userRouteAdminMW.js
@@ -1,12 +1,18 @@
 const userRouteAdminMW = (req, res, next) => {
     //por session.  tiene que ser admin el usuario.
-  req.session.idUser = 0;
+  let usr = 
+  {id:1, esAdmin:0}
+  
+  req.session.userLog = usr;
     
-    
-  if (req.session.idUser == undefined || req.session.idUser <=0) {
+  if (req.session.userLog == undefined ) {
     //no esta logueado.
     res.render("users/login", { titulo: "Mundo Mascota DH-Login" });
-  } else {
+  } else if(req.session.userLog.esAdmin==0)
+  {
+    res.render("noAuthorization", { titulo: "Mundo Mascota DH- No Authorization" });
+  } 
+  else {
     //si esta logueado , ejecutamos next() para seguir con la ejecucion.
     next();
   }
