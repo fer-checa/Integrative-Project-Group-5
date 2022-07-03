fcb82ff27f9144af64ba344dccdeac554917baf5
fer-checa <fernanda.checa2015@gmail.com>
2022-07-03 20:36:10 -0300

    Cmabios en la Navbar segun session de usuario

--- src/middlewares/userLoggedMW.js
+++ src/middlewares/userLoggedMW.js
@@ -3,7 +3,8 @@ function userLoggedMW(req, res, next){
     res.locals.isLogged = false;
 
     if (req.session && req.session.userLogged) {
-        res.locals.isLogged = true; 
+        res.locals.isLogged = true;
+        /* res.locals.userLogged = req.session.userLogged; */
     }
     next();
 }
