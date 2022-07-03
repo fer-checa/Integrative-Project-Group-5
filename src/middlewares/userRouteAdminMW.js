be9b1247266047edf196ae243a5d9cfec74f5887
mamenendez <mamenendez@gmail.com>
2022-07-03 19:36:13 -0300

    merge

--- src/middlewares/userRouteAdminMW.js
+++ src/middlewares/userRouteAdminMW.js
@@ -1,14 +1,7 @@
 const userRouteAdminMW = (req, res, next) => {
     //por session.  tiene que ser admin el usuario.
-<<<<<<< HEAD
   req.session.idUser = 1;
     
-=======
-  let usr = 
-  {id:1, esAdmin:0}
-  
-  req.session.userLog = usr;
->>>>>>> e8929121154cd10105210bf263452de4bf8091a3
     
   if (req.session.userLog == undefined ) {
     //no esta logueado.
