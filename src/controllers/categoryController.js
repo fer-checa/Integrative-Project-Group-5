33e444efdcdd3f6b1bb7a21d1d6997c91dd8da63
MartinEstevez <martin.2003estevez@gmail.com>
2022-07-05 21:22:29 -0300

    CRUD

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { all } = require('../routers/categoryRouter');
 
 const categoryFilePath = path.join(__dirname, '../data/category.json');
 
@@ -17,17 +18,118 @@ const categoryController =
 	},
 
 	create: (req, res) => {
-
-		let newCategory = {
-
-			id: categoria[categoria.length - 1].id + 1,
-			name: req.body.email,
-			family: req.body.familia,
-			user: req.body.user,
-			activo: true,
-
+		let errors = validationResult(req);
+		if (errors.isEmpty()) {
+		  const allCategory = JSON.parse(
+			fs.readFileSync(categoryFilePath, "utf-8")
+		  );
+		  let newCategory = {
+			id: parseInt(allCategory[allCategory.length - 1].id) + 1,
+			nombre: req.body.nombre,
+			activo: req.body.activo == "SI" ? 1 : 0,
+			usuario: "Admin",
+		  };
+		  allCategory.push(newCategory);
+		  fs.writeFileSync(
+			categoryFilePath,
+			JSON.stringify(allCategory, null, " ")
+		  );
+		  res.redirect("/products/category");
+		} else {
+		  const alert = errors.array();
+		  return res.render("products/categoryNew", {
+			titulo: "Mundo Mascota DH-Alta Producto",
+			alert,
+			old: req.body,
+		  });
 		}
-	},
+	  },
 
-}
+	Edit: (req, res) => {
+		const allCategory = JSON.parse(fs.readFileSync(categoryFilePath, "utf-8"));
+		let categoryToEdit = allCategory.find((user) => req.params.id == user.id);
+		res.render("products/category", {
+		  titulo: "Mundo Mascota DH-Editar Producto",
+		  categoryToEdit,
+		});
+	  },
+
+	  update: (req, res) => {
+		let errors = validationResult(req);
+		if (errors.isEmpty()) {
+		  const allCategory = JSON.parse(
+			fs.readFileSync(categoryFilePath, "utf-8")
+		  );
+		  let categoryToEdit = allCategory.find((prod) => req.params.id == prod.id);
+		  let editedCategory = {
+			id: parseInt(req.params.id),
+			nombre: req.body.nombre,
+			usuario: "Admin",
+			activo: req.body.activo == "SI" ? 1 : 0,
+		  };
+		  let indice = allCategory.findIndex((prod) => prod.id == req.params.id);
+		  allCategory[indice] = editedCategory;
+		  fs.writeFileSync(
+			categoryFilePath,
+			JSON.stringify(allCategory, null, " ")
+		  );
+		  res.redirect("/products/category");
+		  }
+
+	  },
+
+	  inactivar: (req, res) => {
+		const todasLasCategorias = JSON.parse(
+		  fs.readFileSync(categoryFilePath, "utf-8")
+		);
+		let unCatego = todasLasCategorias.filter(
+		  (category) => category.id == req.params.id
+		);
+	
+		let editCategory = {
+		  id: req.params.id,
+		  nombre: unProd[0].nombre,
+		  usuario: unProd[0].usuario,
+		  activo: 0,
+		};
+		let indice = todasLasCategorias.findIndex(
+		  (category) => category.id == req.params.id
+		);
+		todasLasCategorias[indice] = editCategory;
+		fs.writeFileSync(
+		  categoryFilePath,
+		  JSON.stringify(todasLasCategorias, null, " ")
+		);
+	
+		res.redirect("/products/category");
+	  },
+	
+	  activar: (req, res) => {
+		const todasLasCategorias = JSON.parse(
+		  fs.readFileSync(categoryFilePath, "utf-8")
+		);
+		let unCatego = todasLasCategorias.filter(
+		  (category) => category.id == req.params.id
+		);
+	
+		let editCategory = {
+		  id: req.params.id,
+		  nombre: unCatego[0].nombre,
+		  usuario: unCatego[0].usuario,
+		  activo: 1,
+		};
+		
+		let indice = todasLasCategorias.findIndex(
+		  (category) => category.id == req.params.id
+		);
+		todasLasCategorias[indice] = editCategory;
+		fs.writeFileSync(
+		  categoryFilePath,
+		  JSON.stringify(todasLasCategorias, null, " ")
+		);
+	
+		res.redirect("/products/category");
+	  },
+	};
+
 module.exports = categoryController;
--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -5,4 +5,12 @@ const categoryController = require ('../controllers/categoryController')
 
 router.get('/',categoryController.todasLasCategorias);
 
+
+router.get('/categoryNew',userRouteAdminMW,categoryController.New);
+router.post('/categoryNew',userRouteAdminMW,validarDatosNew,categoryController.create);
+router.get('/categoryEdit/:id',userRouteAdminMW,categoryController.Edit);
+router.patch('/categoryEdit/:id',userRouteAdminMW,validarDatosNew, categoryController.update); 
+router.post('/categoryInactivar/:id',userRouteAdminMW, categoryController.inactivar);
+router.post('/categoryActivar/:id',userRouteAdminMW, categoryController.activar);
+
 module.exports = router;
