2391abb23a3a239efa4bb95da7d713c3526312e3
fer-checa <fernanda.checa2015@gmail.com>
2022-07-10 01:13:30 -0300

    Mas cambios

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const productsController = require('../controllers/productsControllers');
 
 const guestRouteMW = require('../middlewares/guestRouteMW');
+const authRouteMW  = require('../middlewares/authRouteMW');
 const userLoggedMW = require('../middlewares/userLoggedMW');
 
 router.get('/',productsController.index);
-router.get('/productCart',guestRouteMW,productsController.productCart);
+router.get('/productCart',authRouteMW,productsController.productCart);
 router.get('/productDetail/:id',userLoggedMW,productsController.productDetail);
 router.get('/sucursales',userLoggedMW,productsController.sucursales);
 router.get('/todosLosProductos',userLoggedMW,productsController.todosLosProductos);
