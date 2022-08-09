600da4a1d9a976933d1bde984faf53eac30fea00
Adolfo Wirth <adolfowirth@hotmail.com>
2022-08-09 18:15:12 -0300

    validando desde el front-end categorias y familia

---
+++ public/js/familyProducts.js
@@ -0,0 +1,58 @@
+
+window.addEventListener("load", () => {
+
+    let form = document.querySelector(".form");
+    console.log(form);
+    
+
+    form.addEventListener("submit", (event) => {
+        
+        let errors = [];
+
+        let name = document.querySelector("#nombre");
+        console.log(name);
+        let activo = document.querySelector("#selectActivo");
+        console.log(activo);
+        
+        
+        if (name.value == "") {
+            errors.push("El campo Nombre de Familia no puede estar vacío");
+            name.classList.remove("is-valid");
+            name.classList.add("is-invalid");
+        } else {
+            name.classList.add("is-valid");
+            name.classList.remove("is-invalid");
+            form.activo.focus();
+        };
+
+       
+        
+        if (activo.value == "") {
+            errors.push("El campo Familia Activa no puede estar vacío");
+            activo.classList.remove("is-valid");
+            activo.classList.add("is-invalid");
+        }  else {
+            activo.classList.add("is-valid");
+            activo.classList.remove("is-invalid");
+            
+        };
+
+       
+
+        
+        console.log(errors)
+        if (errors.length > 0) {
+            
+            event.preventDefault();
+            let ulErrors = document.querySelector(".errores");
+            ulErrors.classList.add("alert-warning");
+            ulErrors.innerHTML = "";
+            for (let i = 0; i < errors.length; i++) {
+                ulErrors.innerHTML += "<li>" + errors[i] + "</li>";
+            };
+        } else {
+            alert("La validación fue exitosa")
+            form.submit();
+        }
+    });
+})
