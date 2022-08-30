5001cecb56a0948289e49d555317987ef8531d3d
mamenendez <mamenendez@gmail.com>
2022-08-29 23:44:00 -0300

    Api user y Api user detalle con link

--- src/controllers/api/usersController.js
+++ src/controllers/api/usersController.js
@@ -1,61 +1,59 @@
-const db = require('../../database/models');
+const db = require("../../database/models");
 const sequelize = db.sequelize;
-const moment = require('moment');
-const path = require('path');
+const moment = require("moment");
+const path = require("path");
 const { Op } = require("sequelize");
+const { Sequelize } = require("../../database/models");
 const usersController = {
+ 
+  list: (req, res) => {
+    db.Users.findAll({
+      //attributes: ["id", "name", "email"],
+      // include: ['roles']
+    }).then((usersAux) => {
+      var users = [];
+      for (let index = 0; index < usersAux.length; index++) {
+        users.push({
+             id: usersAux[index].id,
+             name : usersAux[index].name,
+             email: usersAux[index].email,
+             detalle : "/api/users/" + usersAux[index].id
+        });
+      }
+      
 
+      return res.json({
+        meta: {
+          status: 200,
+          Count: users.length,
+          url: "api/users",
+        },
+        data: { users },
+      });
+    });
+  },
 
-    'list': (req, res) => {
-
-        db.Users.findAll({
-            attributes: ['id', 'name','email'],
-            // include: ['roles']
-        })
-        
-                .then(users => {
-                   return res.json({
-                  
-                           meta : {
-                               status: 200,
-                               Count: users.length,
-                               url :"api/users"
-                           },
-
-                            data: 
-                            {users}
-                   })
-    })
-    },
-    
-    'detail': (req, res) => {
-        db.Users.findByPk(req.params.id,
-            {
-                // attributes: ['id', 'name','email','image'],
-                include : ['roles']
-            })
-            .then(users => {
-                let usersRespuesta = {
-                    meta: {
-                        status: 200,
-                        url: 'api/users/:id'
-                    },
-                    // data: {users} 
-                    data : 
-                    {
-                        id : users.id,
-                        name : users.name, 
-                        email : users.email,
-                        image : "/img/usersimage/" + users.image
-                        
-                    }
-                }
-                res.json(usersRespuesta);
-            });
-    }
-    
-   
-}
-
+  detail: (req, res) => {
+    db.Users.findByPk(req.params.id, {
+      //attributes: ['id', 'name','email','image'],
+      include: ["roles"],
+    }).then((user) => {
+      let userAux = {
+        meta: {
+          status: 200,
+          url: "api/users/:id",
+        },
+        // data: {user}
+        data: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: "/img/usersimage/" + user.image,
+        },
+      };
+      res.json(userAux);
+    });
+  },
+};
 
 module.exports = usersController;
